95a24d053e1abefa44a883ed2997b4bf278cb8da
talhasajjad9494 <talha.sajjad9494@gmail.com>
2022-10-03 10:47:34 +0500

    Helper function changes

--- src/main/java/Helpers/Helper.java
+++ src/main/java/Helpers/Helper.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebDriver;
 public class Helper {
     public static WebDriver driver;
     public static void createInstance() {
-        driver = BrowserFactory.startBrowser("chrome", "https://cartlow.com/uae/en");
+        driver = BrowserFactory.startBrowser("chrome", "https://web.cartlow.net/uae/en");
     }
 
 }
